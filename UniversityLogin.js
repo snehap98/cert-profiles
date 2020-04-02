0c9c4e6f6018ced8b70716eed5232075df3eb50c
Sneha P <49320030+snehap98@users.noreply.github.com>
2020-04-02 22:26:21 +0530

    Add files via upload

---
+++ UniversityLogin.js
@@ -0,0 +1,97 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, InputGroup, FormControl, Form } from 'react-bootstrap';
+import Navbar from "./Navbar.js"
+import './Login.css';
+
+
+class UniversityLogin extends Component {
+
+//constructor(props){
+  //super(props);
+//  const {account, contractR} = this.props;
+
+
+      render() {
+        return (
+          <div>
+                <div className="sidebar">
+                  <ul>
+                      <li><a href="#" class="nav-link px-3">
+                          <i class="material-icons icon">
+                            person
+                          </i>
+                          <span class="text">User Profile</span>
+                        </a></li>
+                      <li><a href="#" class="nav-link px-3">
+                          <i class="material-icons icon">
+                            publish
+                          </i>
+                          <span class="text">Upload Certificate</span>
+                        </a></li>
+                      <li><a href="#" class="nav-link px-3">
+                          <i class="material-icons icon">
+                            exit_to_app
+                          </i>
+                          <span class="text">Log Out</span>
+                        </a></li>
+                  </ul>
+              </div>
+
+              <div class="container-fluid mt-5">
+                  <div class="row">
+                      <main role="main" class="col-lg-50 d-flex text-center">
+
+                          <div className="content mr-auto ml-auto"><center>
+                              <h1><center>Profile</center></h1>
+                              <div>
+                                    <form name="a">
+                                    <div>
+                                        Name:<input type="text" className="form-a"/>
+                                    </div>
+                                    <br/>
+                                    <div>
+                                        Email:<input type="text" className="form-a"/>
+                                    </div>
+                                    <br/>
+                                    <div>
+                                        UID  :<input type="text" className="form-a"/>
+                                    </div>
+                                    </form>
+                              </div>
+                              </center>
+                          </div>
+                      </main>
+                    </div>
+
+
+                    <div class="row">
+                      <main role="main" class="col-lg-100">
+                          <div id="content"><center>
+                              <h1><center>Upload Certificate</center></h1>
+                              <div>
+                                  <br/>
+                                  <form name="b">
+                                      <div>
+                                          Student UID<input type="text" name="cid" className="form-b"/>
+                                      </div>
+                                      <br/>
+                                      <div>
+                                          <center><button id="snd" type="submit">Upload Certificate</button></center>
+                                      </div>
+                                  </form>
+                              </div>
+                              </center>
+                          </div>
+
+                      </main>
+                  </div>
+                </div>
+              </div>
+              );
+            }
+
+
+}
+
+export default UniversityLogin;
